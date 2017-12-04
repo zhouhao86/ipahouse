ee58ec9e33c8579983b84d9f0ff132465cb5dd4e
zhouhao86 <32256952+zhouhao86@users.noreply.github.com>
2017-12-04 13:54:37 +0800

    Update 58wallet.js

--- 58wallet.js
+++ 58wallet.js
@@ -1,5 +1,7 @@
 function JBIwallet() {
 
+     this.callbackId= 0;
+     this.callbackArray = [];
 
 }
 JBIwallet.prototype = {
@@ -10,6 +12,25 @@ JBIwallet.prototype = {
        var str = JSON.stringify(jsonObj);
        window.postMessage(str)
    }
+   
+   GetAccountsCallBack:function(callback) {
+       var jsonObj =  new Object();
+       jsonObj.method = "GetAccounts";
+       jsonObj.callbackId= this.callbackId;
+       this.callbackArray[this.callbackId++] = callback;
+       var str = JSON.stringify(jsonObj);
+       window.postMessage(str)
+      
+   }
+
+   exeCallback:function(param) {
+      var jsonObj = JSON.parse(param);
+      var callback = this.callbackArray[param.callbackId];
+      if(callback && typeof(callback) === "function") {
+         callback(param.data);
+      }
+   }
+   
 }
 
 
